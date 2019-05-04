148165a4585c2a762fa5d06bbe067c4349b641ce
SKYSCRAPERS1999 <386333667@qq.com>
2019-05-03 21:09:43 -0700

    Modify mouse callback

--- src/callback.h
+++ src/callback.h
@@ -20,15 +20,6 @@ void mouse_button_callback(GLFWwindow *window, int button, int action, int mods)
     if (button == GLFW_MOUSE_BUTTON_LEFT) {
         if (action == GLFW_PRESS) {
             mouse_down = true;
-
-            double xpos = grid.cursor_pos[0];
-            double ypos = grid.cursor_pos[1];
-
-            int row = int(NUMROW - NUMROW * ypos / double(WINDOW_HEIGHT));
-            int col = int(NUMCOL * xpos / double(WINDOW_WIDTH));
-
-            double den = grid.getDensity(col, row);
-            grid.setDensity(col, row, std::max(den + 25, 100.0));
         } else if (action == GLFW_RELEASE) {
             mouse_down = false;
         }
