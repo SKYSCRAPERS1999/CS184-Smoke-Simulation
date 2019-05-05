806830ffaeb642a37c2c1bbad0f2dd80746b8d96
yhZhai <862989569@qq.com>
2019-05-04 17:04:52 -0700

    Comment all nanogui sentence

--- src/main.cpp
+++ src/main.cpp
@@ -89,19 +89,19 @@ int main() {
     if (!glfwInit()) {
         return -1;
     }
-    glfwSetTime(0);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_SAMPLES, 0);
-    glfwWindowHint(GLFW_RED_BITS, 8);
-    glfwWindowHint(GLFW_GREEN_BITS, 8);
-    glfwWindowHint(GLFW_BLUE_BITS, 8);
-    glfwWindowHint(GLFW_ALPHA_BITS, 8);
-    glfwWindowHint(GLFW_STENCIL_BITS, 8);
-    glfwWindowHint(GLFW_DEPTH_BITS, 24);
-    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+//    glfwSetTime(0);
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+//    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+//    glfwWindowHint(GLFW_SAMPLES, 0);
+//    glfwWindowHint(GLFW_RED_BITS, 8);
+//    glfwWindowHint(GLFW_GREEN_BITS, 8);
+//    glfwWindowHint(GLFW_BLUE_BITS, 8);
+//    glfwWindowHint(GLFW_ALPHA_BITS, 8);
+//    glfwWindowHint(GLFW_STENCIL_BITS, 8);
+//    glfwWindowHint(GLFW_DEPTH_BITS, 24);
+//    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
 
     window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Smoke Simulation", nullptr, nullptr);
     if (!window) {
