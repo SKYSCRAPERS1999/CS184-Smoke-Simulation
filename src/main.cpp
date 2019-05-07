b79eb40a953d2dcd9cbce4eca12563ea274bf5ef
yhZhai <862989569@qq.com>
2019-05-06 21:10:16 -0700

    Bug fix for vector field

--- src/main.cpp
+++ src/main.cpp
@@ -400,7 +400,8 @@ int main() {
                     int vertexColorLocation = glGetUniformLocation(shader_program, "ourColor");
                     glUniform4f(vertexColorLocation, rgb.x, rgb.y, rgb.z, 1.0f);
                     glBindVertexArray(VAOs[index]);
-                    glDrawArrays(GL_TRIANGLES, 0, 6);
+                    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+//                    glDrawArrays(GL_TRIANGLES, 0, 6);
                 }
             }
         } else {
